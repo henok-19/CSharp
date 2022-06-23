f9a71ca638ec8979ac0f4908dd312b70c2562be5
henok-19 <107881513+henok-19@users.noreply.github.com>
2022-06-23 19:37:34 +0300

    Add files via upload

---
+++ CSharp_Assignment_2/CSharp_Assignment_2/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+
+    public static void Main(string[] args)
+    {
+        int a;
+        string s;
+        do
+        {
+            Console.WriteLine("Enter a number");
+            a = Convert.ToInt32(Console.ReadLine());
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("is  Even");
+            }
+            else
+            {
+                Console.WriteLine(" is Odd");
+            }
+            Console.WriteLine("Do you want to continue?(Yes/No)");
+            s = Console.ReadLine();
+            if (s == "no")
+                Console.WriteLine("Bye!!!");
+
+        } while (s == "yes");
+    }
+}
