b5db67aabd75120f8aae8c48134fc5723b420163
Henok <henok.feleke19@gmail.com>
2022-07-06 23:34:43 -0600

    Add Inventory form

---
+++ WinApp/Add Inventory.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace WinApp
+{
+    partial class Add_Inventory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_number = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_inventoryno = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_objectname = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_count = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_price = new System.Windows.Forms.TextBox();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.btn_cancel = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number";
+            // 
+            // txt_number
+            // 
+            this.txt_number.Location = new System.Drawing.Point(42, 63);
+            this.txt_number.Name = "txt_number";
+            this.txt_number.Size = new System.Drawing.Size(159, 22);
+            this.txt_number.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(204, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Date";
+            // 
+            // txt_inventoryno
+            // 
+            this.txt_inventoryno.Location = new System.Drawing.Point(450, 63);
+            this.txt_inventoryno.Name = "txt_inventoryno";
+            this.txt_inventoryno.Size = new System.Drawing.Size(345, 22);
+            this.txt_inventoryno.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(447, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Inventory Number";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(39, 119);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Object Name";
+            // 
+            // txt_objectname
+            // 
+            this.txt_objectname.Location = new System.Drawing.Point(42, 147);
+            this.txt_objectname.Name = "txt_objectname";
+            this.txt_objectname.Size = new System.Drawing.Size(707, 22);
+            this.txt_objectname.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(39, 211);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Count";
+            // 
+            // txt_count
+            // 
+            this.txt_count.Location = new System.Drawing.Point(42, 243);
+            this.txt_count.Name = "txt_count";
+            this.txt_count.Size = new System.Drawing.Size(198, 22);
+            this.txt_count.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(243, 211);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 16);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Price";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // txt_price
+            // 
+            this.txt_price.Location = new System.Drawing.Point(246, 243);
+            this.txt_price.Name = "txt_price";
+            this.txt_price.Size = new System.Drawing.Size(198, 22);
+            this.txt_price.TabIndex = 11;
+            // 
+            // btn_add
+            // 
+            this.btn_add.Location = new System.Drawing.Point(42, 317);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(198, 55);
+            this.btn_add.TabIndex = 12;
+            this.btn_add.Text = "Add";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // btn_cancel
+            // 
+            this.btn_cancel.Location = new System.Drawing.Point(246, 317);
+            this.btn_cancel.Name = "btn_cancel";
+            this.btn_cancel.Size = new System.Drawing.Size(198, 55);
+            this.btn_cancel.TabIndex = 13;
+            this.btn_cancel.Text = "Cancel";
+            this.btn_cancel.UseVisualStyleBackColor = true;
+            this.btn_cancel.Click += new System.EventHandler(this.btn_cancel_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(207, 63);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(237, 22);
+            this.dateTimePicker1.TabIndex = 14;
+            // 
+            // Add_Inventory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(962, 450);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.btn_cancel);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.txt_price);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txt_count);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txt_objectname);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_inventoryno);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_number);
+            this.Controls.Add(this.label1);
+            this.Name = "Add_Inventory";
+            this.Text = "Add_Inventory";
+            this.Load += new System.EventHandler(this.Add_Inventory_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_number;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_inventoryno;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_objectname;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_count;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt_price;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.Button btn_cancel;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
---
+++ WinApp/Add Inventory.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WinApp.Model_Item;
+
+namespace WinApp
+{
+    public partial class Add_Inventory : Form
+    {
+        public Add_Inventory()
+        {
+            InitializeComponent();
+        }
+
+        private void Add_Inventory_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            Item item = new Item();
+
+            item.number = Convert.ToInt32(txt_number.Text);
+            item.dates = Convert.ToDateTime(dateTimePicker1.Value);
+            item.inventory_number = Convert.ToInt32(txt_number.Text);
+            item.object_name = txt_objectname.Text;
+            item.count = Convert.ToInt32(txt_count.Text);
+            item.price = Convert.ToDouble(txt_price.Text);
+
+            MessageBox.Show(item.save() + " has been Successfully Added!!!");
+        }
+
+        private void btn_cancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    
+    }
+}
